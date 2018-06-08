4ef024770b1bb568b151190d75e12f7dda230ab4
Alternanza <alternanza.5bi@gmail.com>
2018-06-08 12:52:44 +0200

    Merge branch 'master' of https://github.com/Alternanza5ABi/MuradiCatania
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/alternanza/muradicatania/MapsActivity.java
+++ app/src/main/java/com/example/alternanza/muradicatania/MapsActivity.java
@@ -3,19 +3,13 @@ package com.example.alternanza.muradicatania;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 {
